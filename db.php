35f8223dc52f066f638751d5dbfa2949eb19bb8c
Alan <ibnualannuari@gmail.com>
2025-01-04 00:20:42 +0700

    baru1

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // db.php - Untuk koneksi ke database
 $host = 'localhost';  // Ganti dengan host database, biasanya 'localhost'
-$username = 'root';   // Ganti dengan username database
-$password = '';       // Ganti dengan password database (kosong untuk XAMPP)
-$dbname = 'properties'; // Nama database yang digunakan
+$username = 'u772674178_properties';   // Ganti dengan username database
+$password = 'Propertiesalan123';       // Ganti dengan password database (kosong untuk XAMPP)
+$dbname = 'u772674178_properties'; // Nama database yang digunakan
 
 // Membuat koneksi ke database
 $conn = new mysqli($host, $username, $password, $dbname);

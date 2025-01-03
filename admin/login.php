296d78ac61ed19e46d0f9603ac422774e13a02ed
Alan <ibnualannuari@gmail.com>
2025-01-04 01:50:38 +0700

    rubah login

--- admin/login.php
+++ admin/login.php
@@ -33,8 +33,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
-buat halaman bagus dari boostrap dan warna jangan hitam
-
 </html>
 
 <!-- PHP Login Logic -->

4b99fe06b115cbeb299183f535b5ba26131eceae
Alan <ibnualannuari@gmail.com>
2025-01-06 04:53:56 +0700

    perbarui index

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ $result_best = $conn->query($sql_best);
     </header>
 
     <!-- Hero Section -->
-    <section class="hero-section d-flex align-items-center" style="margin-top: 100px; margin-bottom: 40px; height: auto;">
+    <section class="hero-section d-flex align-items-center" style="margin-top: 100px; margin-bottom: 30px; height: auto;">
         <div class="container d-flex justify-content-center">
             <div class="row align-items-center">
                 <!-- Gambar -->

cfbc49cd69be62d5581a8bfc99c4b0d63266dba6
Alan <ibnualannuari@gmail.com>
2025-01-06 04:17:57 +0700

    perbarui

--- index.php
+++ index.php
@@ -283,6 +283,7 @@ $result_best = $conn->query($sql_best);
             </footer>
             <script src="js/script.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+            
             <script>document.addEventListener('DOMContentLoaded', function () {
     const resizeCards = () => {
         const cards = document.querySelectorAll('.col-lg-4.col-md-6.col-sm-6.col-12');

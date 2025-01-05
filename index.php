315cb64a8b82aa349721a36001324b791326a764
Alan <ibnualannuari@gmail.com>
2025-01-06 05:49:13 +0700

    index

--- index.php
+++ index.php
@@ -34,9 +34,9 @@ $result_best = $conn->query($sql_best);
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark position-fixed w-100">
-            <div class="container d-flex justify-content-between align-items-center">
-                <!-- Brand Logo di sebelah kiri -->
-                <a class="navbar-brand d-flex align-items-center" href="index">
+            <div class="container">
+                <!-- Brand di sisi kanan pada layar kecil, kiri pada layar besar -->
+                <a class="navbar-brand d-flex align-items-center ms-auto" href="index">
                     <img src="assets/images/logo.png" alt="Logo" class="me-2" style="height: 40px;">
                     <span class="fs-6 d-inline d-md-none">PT MITRA</span> <!-- Nama kecil untuk layar kecil -->
                     <span class="fs-5 d-none d-md-inline">PT MITRA USAHA SYARIAH</span>
@@ -51,19 +51,21 @@ $result_best = $conn->query($sql_best);
 
                 <!-- Navigation Links -->
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Property
-                        </a>
-                        <ul class="dropdown-menu custom-dropdown" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="new_property">Property Terbaru</a></li>
-                            <li><a class="dropdown-item" href="best_property">Property Terbaik</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item"><a href="#about-us" class="nav-link">Tentang Kami</a></li>
-                    <li class="nav-item"><a href="contact" class="nav-link">Kontak</a></li>
-                    <li class="nav-item"><a href="admin/login.php" target="_blank" class="nav-link">Admin</a></li>
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="index" class="nav-link">Home</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Property
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="new_property">Property Terbaru</a></li>
+                                <li><a class="dropdown-item" href="best_property">Property Terbaik</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a href="#about-us" class="nav-link">Tentang Kami</a></li>
+                        <li class="nav-item"><a href="contact" class="nav-link">Kontak</a></li>
+                        <li class="nav-item"><a href="admin/login.php" target="_blank" class="nav-link">Admin</a></li>
                     </ul>
                 </div>
             </div>

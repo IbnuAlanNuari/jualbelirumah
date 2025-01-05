6f312b580c4c7c8c667d35cc3f535e24cccc640f
Alan <ibnualannuari@gmail.com>
2025-01-06 04:47:37 +0700

    perbarui index

--- index.php
+++ index.php
@@ -73,11 +73,11 @@ $result_best = $conn->query($sql_best);
     </header>
 
     <!-- Hero Section -->
-    <section class="hero-section d-flex align-items-center" style="margin-top: 80px;">
-        <div class="container">
+    <section class="hero-section d-flex align-items-center" style="margin-top: 80px; height: 100vh;">
+        <div class="container d-flex justify-content-center">
             <div class="row align-items-center">
                 <!-- Gambar -->
-                <div class="col-md-6 align-items-center">
+                <div class="col-md-12 text-center">
                     <img src="assets/images/display.jpg" alt="Hero Image" class="img-fluid rounded shadow">
                 </div>
             </div>

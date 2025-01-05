7bf1d1af411bd15e843e183699e079d1bbb4a569
Alan <ibnualannuari@gmail.com>
2025-01-05 10:59:28 +0700

    perbarui

--- admin/add_property.php
+++ admin/add_property.php
@@ -130,7 +130,7 @@ if (isset($_GET['edit'])) {
             <div class="container d-flex justify-content-between align-items-center">
                 <!-- Brand Logo -->
                 <a class="navbar-brand d-flex align-items-center" href="index">
-                    <img src="assets/images/logo.png" alt="Logo" class="me-2" style="height: 40px;">
+                    <img src="../assets/images/logo.png" alt="Logo" class="me-2" style="height: 40px;">
                     <span class="fs-6 d-inline d-md-none">PT MITRA</span> <!-- Nama kecil untuk layar kecil -->
                     <span class="fs-5 d-none d-md-inline">PT MITRA USAHA SYARIAH</span>
                     <!-- Nama lengkap untuk layar besar -->
